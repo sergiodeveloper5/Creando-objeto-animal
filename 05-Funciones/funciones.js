987c7614f93cc20887b38313d89b8de92c1b99b6
sergiodeveloper5 <156307738+sergiodeveloper5@users.noreply.github.com>
2024-02-15 11:53:23 +0100

    Creando objeto animal

---
+++ 05-Funciones/funciones.js
@@ -0,0 +1,15 @@
+function suma(a, b) {
+    return a + b;
+}
+
+let resultadoSuma = suma(23, 46);
+console.log(resultadoSuma);
+
+function resta(a, b) {
+    return a - b;
+}
+
+let resultadoResta = resta(64, 23);
+console.log(resultadoResta);
+
+
